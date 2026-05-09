80cd648af3b9b7660bfd2307e7f3a29fc00ce0ec
apxllo1 <bravounlimitaura@gmail.com>
2026-05-09 10:44:18 -0500

    Add 'Misc' page to DashboardPage enum and mappings

--- src/store/models/dashboard.model.ts
+++ src/store/models/dashboard.model.ts
@@ -2,6 +2,7 @@ export enum DashboardPage {
 	Home = "home",
 	Apps = "apps",
 	Scripts = "scripts",
+	Misc = "misc",
 	Options = "options",
 }
 
@@ -18,12 +19,14 @@ export const PAGE_TO_INDEX: Record<DashboardPage, number> = {
 	[DashboardPage.Home]: 0,
 	[DashboardPage.Apps]: 1,
 	[DashboardPage.Scripts]: 2,
-	[DashboardPage.Options]: 3,
+	[DashboardPage.Misc]: 3,
+	[DashboardPage.Options]: 4,
 };
 
 export const PAGE_TO_ICON: Record<DashboardPage, string> = {
 	[DashboardPage.Home]: "rbxassetid://8992031167",
 	[DashboardPage.Apps]: "rbxassetid://8992031246",
 	[DashboardPage.Scripts]: "rbxassetid://8992030918",
+	[DashboardPage.Misc]: "rbxassetid://84061544875190",
 	[DashboardPage.Options]: "rbxassetid://8992031056",
 };
